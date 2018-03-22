48b25c857d364eca9bbcf7f07acb977d05216a81
Matt Ellis <95ellismle@gmail.com>
2018-03-22 18:19:28 +0000

    Started to add a error message.

--- mysite/Transform/views.py
+++ mysite/Transform/views.py
@@ -9,9 +9,12 @@ def index(request):
     else:
         equation_in = request.POST['equation_in']
         Trans = Transform(equation_in)
+        steps_taken = [i for i in Trans.eq.steps_taken.split("**#") if i]
+        step_text = [i for i in Trans.eq.step_text.split('**#') if i]
         context = {'eq_in':Trans.eq.eq_in,
-                   'print_eq': True,
+                   'print_eq': Trans.eq.eq_in.count("\\") > 0,
                     "test":"",
-                    'steps':zip(Trans.eq.steps_taken.split('**#'),Trans.eq.step_text.split('**#')),
+                    'steps':zip(steps_taken,step_text),
+                    'print_steps':len(steps_taken) > 1,
                     }
     return render(request, "Transform/index.html",context)
